1c5305b2d2b0cb04c1436e1a25bdbebf630acf67
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-04-22 00:21:48 +0530

    Setup Tailwind CSS

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { Counter } from './features/counter/Counter';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-blue-700 text-9xl text-center">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
