d4a379b64313e0426b69c5531670be8e996f2912
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-05-10 13:53:49 +0530

    Render Netflix logo and avatar images

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import NetflixLogo from "../assets/netflix-logo.png";
+import NetflixAvatar from "../assets/netflix-avatar.png";
 
 function Nav() {
   return (
     <div>
       <nav>
         <div>
-          
+          <img 
+            className=""
+            src={NetflixLogo} 
+            alt="Netflix logo">
+          </img>
+
+          <img 
+            className="" 
+            src={NetflixAvatar}
+            alt="Netflix avatar">
+          </img>
         </div>
       </nav>
     </div>
