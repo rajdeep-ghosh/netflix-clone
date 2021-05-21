3fda0fda47dff4e3829de414fbd7099114cb28fc
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-05-21 20:21:54 +0530

    Add dummy banner contents

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,8 +2,22 @@ import React from "react";
 
 export default function Banner() {
   return (
-    <div>
-      <h1>Test</h1>
-    </div>
+    <header
+      className="h-96 relative object-contain text-white"
+      style={{
+        backgroundSize: "cover",
+        backgroundColor: "black",
+        backgroundPosition: "center center"
+      }}
+    >
+      <div>
+        <h1>Movie Name</h1>
+        <div>
+          <button>Play</button>
+          <button>My List</button>
+        </div>
+        <h1>This is a test description</h1>
+      </div>
+    </header>
   );
 }
