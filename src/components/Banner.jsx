63ba7ea23c3881fc840a3a320c8e27586ef7527e
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-05-21 22:14:33 +0530

    Style banner description and create truncate function

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import "./Banner.css";
 
 export default function Banner() {
+  function truncate(string, n) {
+    return string?.length > n ? `${string.substr(0, n - 1)} ...` : string;
+  }
+
   return (
     <header
       className="h-96 relative object-contain text-white"
       style={{
         backgroundSize: "cover",
         backgroundColor: "black",
-        backgroundPosition: "center center"
+        backgroundPosition: "center center",
       }}
     >
       <div className="ml-8 pt-36 h-48">
@@ -21,7 +25,17 @@ export default function Banner() {
             My List
           </button>
         </div>
-        <h1 className="text-sm w-[45rem] max-w-sm h-20 pt-4">This is a test description</h1>
+        <h1 className="text-sm w-[45rem] max-w-sm h-20 pt-4">
+          {truncate(
+            `This is a test description. This is a test description. This is a test description. This is a test description.
+          This is a test description. This is a test description. This is a test description. This is a test description.
+          This is a test description. This is a test description. This is a test description. This is a test description.
+          This is a test description. This is a test description. This is a test description. This is a test description.
+          This is a test description. This is a test description. This is a test description. This is a test description.
+          This is a test description. This is a test description.`,
+            130
+          )}
+        </h1>
       </div>
 
       <div className="banner--fadeBottom h-28 mt-20" />
