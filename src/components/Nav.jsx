0066fa43eb6a8bc6b38a583f78d48bb21a797fb7
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-05-21 14:18:10 +0530

    Add navbar transition animation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NetflixLogo from "../assets/netflix-logo.png";
 import NetflixAvatar from "../assets/netflix-avatar.png";
 
 export default function Nav() {
+  const [showNavbar, handleShow] = useState(false);
+  
+  function transitionNavbar() {
+    if(window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavbar);
+    return () => window.removeEventListener("scroll", transitionNavbar);
+  }, []);
+
   return (
-    <div className="fixed top-0 p-5 w-full h-20 z-[1] netflix__black transition ease-in duration-500">
+    <div className={`fixed top-0 p-5 w-full h-20 z-[1] ${showNavbar && `netflix__black`} transition ease-in duration-500`}>
       <nav className="flex justify-between">
         <img className="w-28 fixed left-0 top-3 object-contain pl-5 cursor-pointer" src={NetflixLogo} alt="Netflix logo" />
         <img className="w-6 fixed right-5 cursor-pointer" src={NetflixAvatar} alt="Netflix avatar" />
