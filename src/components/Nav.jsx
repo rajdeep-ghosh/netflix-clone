05ec3bc27ed76e252bb9c2d549d7b8f92bc23a68
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-05-21 14:11:58 +0530

    Complete navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,11 +4,11 @@ import NetflixAvatar from "../assets/netflix-avatar.png";
 
 export default function Nav() {
   return (
-    <div>
-      <div>
-        <img className="fixed w-24" src={NetflixLogo} alt="Netflix logo" />
-        <img className="fixed w-6" src={NetflixAvatar} alt="Netflix avatar" />
-      </div>
+    <div className="fixed top-0 p-5 w-full h-20 z-[1] netflix__black transition ease-in duration-500">
+      <nav className="flex justify-between">
+        <img className="w-28 fixed left-0 top-3 object-contain pl-5 cursor-pointer" src={NetflixLogo} alt="Netflix logo" />
+        <img className="w-6 fixed right-5 cursor-pointer" src={NetflixAvatar} alt="Netflix avatar" />
+      </nav>
     </div>
   );
 }
