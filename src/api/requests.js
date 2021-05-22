6cef5d561e0034249410078e54f4ae338b2ee41c
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-05-22 22:03:15 +0530

    Create requests.js and configure API endpoints

---
+++ src/api/requests.js
@@ -0,0 +1,16 @@
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchAnimationMovies: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+  fetchScienceFictionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumantaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
