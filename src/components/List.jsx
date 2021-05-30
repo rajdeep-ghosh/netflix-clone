2a9464babb147236e7f538b380a2160d7eda4da0
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-05-30 17:34:44 +0530

    Position list items

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -19,8 +19,8 @@ export default function List(props) {
   return (
     <div className="px-12 py-6 w-full flex bg-gradient-to-b from-netflix-red-light to-netflix-black">
       <div className="mr-8 relative big__list">
-        <h2 className="font-mulish font-extrabold text-xl mb-8">{props.title}</h2>
-        <div>
+        <h2 className="font-mulish font-extrabold text-2xl text-white leading-5 mb-8">{props.title}</h2>
+        <div className="flex overflow-y-hidden overflow-x-scroll p-2 -ml-2 -mt-2 list__posters">
           {contents.map((content) => {
             return <ListItem key={content.id} content={content} />;
           })}
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import "./styles/ListItem.css";
 
 export default function ListItem(props) {
   const posterBaseURL = "https://image.tmdb.org/t/p/w500";
 
   return (
-    <div>
+    <div className="list__item h-[300px] mr-5 rounded-2xl overflow-hidden relative cursor-pointer shadow-md">
       <img
+        className="w-full h-full object-cover"
+        loading="lazy"
         src={`${posterBaseURL}${props.content.poster_path}`}
-        alt={props.content.name}
+        alt={props.content?.title || props.content?.original_title || props.content?.name || props.content?.original_name}
       />
     </div>
   );
