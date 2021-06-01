f36473e71ce141e721223eaa41f79bdb0e17cf3e
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-06-02 04:05:38 +0530

    Add dynamic copyright year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import tmdbIcon from "../assets/tmdb.svg";
 
 export default function Footer() {
+  const year = new Date().getFullYear();
+
   return (
     <div className="bg-netflix-red-light flex flex-col items-center justify-center p-4 pt-24">
       <img className="w-32" src={tmdbIcon} alt="TMDB icon" />
       <div className="pt-4 font-work-sans font-medium text-base text-white">
         <p className="whitespace-nowrap w-full">
-          &copy; 2021 Rajdeep Ghosh | 
+          &copy; {year} Rajdeep Ghosh | 
           <a href="https://github.com/rajdeep-ghosh/netflix-clone" target="_blanck"> GitHub</a>
         </p>
       </div>
