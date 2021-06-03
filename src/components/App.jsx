60169ad148645c215a187633d58a80d38f193624
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-06-03 06:21:55 +0530

    Implement ScrollToTop on route change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,12 @@ import Nav from "./Nav";
 import HomeScreen from "./HomeScreen";
 import Footer from "./Footer";
 import ShowDetails from "./ShowDetails";
+import ScrollToTop from "./services/ScrollToTop";
 
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="antialiased">
         <Nav />
         <Switch>
